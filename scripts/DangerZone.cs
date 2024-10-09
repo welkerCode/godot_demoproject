69d7ee02a036eb43433df862b3c17519f5d1d930
Taylor Welker <taylormaxwelker@gmail.com>
2024-10-08 21:04:03 -0700

    Started creating pickups.

--- scripts/DangerZone.cs
+++ scripts/DangerZone.cs
@@ -1,25 +1,34 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
-public partial class DangerZone : Area2D
+public partial class DangerZone : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//+= OnBodyEnteredSignal;
-		//Callable myCallable = new Callable(this, nameof(OnBodyEntered));
-
-		// Ensure the player node is referenced correctly.
-        //var player = GetNode<Player>("../Player"); // Update the path accordingly
-        //DangerZone.Connect("body_entered", myCallable);
+		GD.Print("Danger Zone ready");
+		Connect("body_entered", new Callable(this, nameof(_on_area_2d_body_entered)));
 	}
 
-	private void OnBodyEnteredSignal()
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//public override void _Process(double delta)
+	//{
+	//}
+	
+	private void _on_area_2d_body_entered(Node body)
 	{
-		//GD.Print("Dead!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		//if (body is Player player)
-		//{
-		//	player.OnDeathAreaBodyEntered();
-		//}
+		GD.Print("Body: " + body + " has entered...");
+		GD.Print("Body Class: " + body.GetClass());
+		if (body is CharacterBody2D)
+		{
+			GD.Print("A CharacterBody2D has entered...");
+			if (body is Player){
+				GD.Print("The Player has entered...time to kill them...");
+				Player p = body as Player;
+				p.reset_player();
+			}
+		}
 	}
+
 }
